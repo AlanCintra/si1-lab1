cc5c275d2566afad4a705adfd706714f9b1bd634
AlanCintra <alan.gomes@ccc.ufcg.edu.br>
2014-10-29 20:28:52 -0300

    dinamica completa

--- dinamica/js/funcoes.js
+++ dinamica/js/funcoes.js
@@ -1,6 +1,36 @@
-$(document).ready(function() {
-    $('#adiciona').click(function() {
-        var conteudo = $('#conteudo').val();
-        $('#conteudos').append('<li>' + conteudo + '</li>');
+$(function() {
+    $('#conteudo').keypress(function(e) {
+        if (e.which != 13) {
+        } else {
+            addConteudo();
+            e.preventDefault();
+        }
     })
 })
+
+function addConteudo() {
+    var text = "<assunto>" + $('#conteudo').val() + "</assunto>";
+    var addButton = "<button class='btn btn-inverse' type='button' id='mover'>Aprendido</button>";
+    var remButton = "<button class='btn btn-inverse' type='button' id='excluir'>Excluir</button>"
+    var conteudo = '<li id="items">' + text + remButton + addButton + '</li>';
+    $('#conteudos').append(conteudo);
+    $('#conteudo').val('');
+    $('#mover').bind('click', mover());
+    $('#excluir').bind('click', excluir());
+}
+
+function mover() {
+    $('#conteudos').on('click', '#mover', function() {
+        var item = $(this).parent();
+        var assunto = item.find("assunto").text();
+        $('#aprendidos').append('<li>' + assunto + '</li>');
+        item.remove();
+        assunto.remove();
+    })
+}
+
+function excluir() {
+    $('#conteudos').on('click', '#excluir', function() {
+        $(this).parent().remove();
+    })
+}
